44cc81609db1fdcbd86d9d376d6476dcce798d0e
liluo <i@liluo.org>
2013-03-01 16:23:27 +0800

    DEV: added BlobHelper; added FileBlob unittest

---
+++ linguist/libs/blob_helper.py
@@ -0,0 +1,436 @@
+# -*- coding: utf-8 -*-
+
+import re
+import urllib
+import mimetypes
+from os.path import realpath, dirname, splitext, basename, join
+
+import yaml
+import chardet
+from pygments import lexers 
+from pygments import highlight
+from pygments.formatters import HtmlFormatter
+
+from language import Language
+from generated import Generated
+
+MEGABYTE = 1024 * 1024
+
+DIR = dirname(realpath(__file__))
+VENDOR_PATH = join(DIR, "vendor.yml")
+VENDORED_PATHS = yaml.load(open(VENDOR_PATH))
+VENDORED_REGEXP = re.compile('|'.join(VENDORED_PATHS))
+
+if not mimetypes.inited:
+    mimetypes.init()
+    mimetypes.add_type('application/x-ruby', '.rb')
+    mimetypes.add_type('application/x-python', '.py')
+
+class BlobHelper(object):
+    """
+    BlobHelper is a mixin for Blobish classes that respond to "name",
+      "data" and "size" such as Grit::Blob.
+    """
+
+    @property
+    def ext_name(self):
+        """
+        Public: Get the extname of the path
+
+        Examples
+          blob(name='foo.rb').extname
+          # => '.rb'
+
+        Returns a String
+        """
+        return splitext(self.name)[1]
+
+    @property
+    def _mime_type(self):
+        """
+        Public: Get the actual blob mime type
+
+        Examples
+
+          # => 'text/plain'
+          # => 'text/html'
+
+        Returns a mime type String.
+        """
+        if hasattr(self, '__mime_type'):
+            return self.__mime_type 
+        _type, _encoding = mimetypes.guess_type(self.name)
+        self.__mime_encodeing = _encoding
+        self.__mime_type = _type
+        return _type
+
+    @property
+    def mime_type(self):
+        return self._mime_type or 'text/plain'
+
+    @property
+    def is_binary_mime_type(self):
+        """
+        Internal: Is the blob binary according to its mime type
+
+        Return true or false
+        """
+        self._mime_type
+        return bool(self.__mime_encodeing)
+
+    @property
+    def is_likely_binary(self):
+        """
+        Internal: Is the blob binary according to its mime type,
+        overriding it if we have better data from the languages.yml
+        database.
+
+        Return true or false
+        """
+        return self.is_binary_mime_type and not Language.find_by_filename(self.name)
+
+    @property
+    def content_type(self):
+        """
+        Public: Get the Content-Type header value
+
+        This value is used when serving raw blobs.
+
+        Examples
+
+          # => 'text/plain; charset=utf-8'
+          # => 'application/octet-stream'
+
+        Returns a content type String.
+        """
+        if hasattr(self, '_content_type'):
+            return self._content_type
+
+        if self.is_binary_mime_type or self.is_binary:
+            self._content_type = self.mime_type
+        else:
+            encoding = self.encoding
+            self._content_type = "text/plain; charset=%s" % encoding.lower() if encoding else "text/plain"
+        return self._content_type
+
+    @property
+    def disposition(self):
+        """
+        Public: Get the Content-Disposition header value
+
+        This value is used when serving raw blobs.
+
+          # => "attachment; filename=file.tar"
+          # => "inline"
+
+        Returns a content disposition String.
+        """
+        if self.is_text or self.is_image:
+            return 'inline'
+        elif self.name is None:
+            return 'attachment'
+        else:
+            return 'attachment; filename=%s' % urllib.quote_plus(basename(self.name))
+
+    @property
+    def encoding(self):
+        if self.detect_encoding:
+            return self.detect_encoding.get('encoding')
+
+    @property
+    def detect_encoding(self):
+        """
+        Try to guess the encoding
+
+        Returns: a hash, with :encoding, :confidence, :type
+                 this will return nil if an error occurred during detection or
+                 no valid encoding could be found
+        """
+        if hasattr(self, '_detect_encoding'):
+            return self._detect_encoding
+
+        if self.data:
+            self._detect_encoding = chardet.detect(self.data)
+        return self._detect_encoding
+
+    @property
+    def is_image(self):
+        """
+        Public: Is the blob a supported image format?
+
+        Return true or false
+        """
+        return self.ext_name in ('.png', '.jpg', '.jpeg', '.gif')
+
+    @property
+    def is_solid(self):
+        """
+        Public: Is the blob a support 3D model format?
+        
+        Return true or false
+        """
+        return self.ext_name in ('.stl', '.obj')
+
+    @property
+    def is_text(self):
+        """
+        Public: Is the blob text?
+
+        Return true or false
+        """
+        return not self.is_binary
+
+    @property
+    def is_binary(self):
+        if self.data is None:
+            # Large blobs aren't even loaded into memory
+            return True
+        elif self.data == "":
+            # Treat blank files as text
+            return False
+        elif self.encoding is None:
+            # Charlock doesn't know what to think
+            return True
+        else:
+            # If Charlock says its binary
+            # TODO
+            return self.detect_encoding.get('type') == 'binary'
+
+    @property
+    def is_large(self):
+        """
+        Public: Is the blob too big to load?
+
+        Return false or true
+        """
+        return self.size > MEGABYTE
+
+    @property
+    def loc(self):
+        """
+        Public: Get number of lines of code
+
+        Requires Blob#data
+
+        Returns Integer
+        """
+        return len(self.lines)
+
+    @property
+    def sloc(self):
+        """
+        Public: Get number of source lines of code
+
+        Requires Blob#data
+
+        Returns Integer
+        """
+        return len(filter(re.compile('\S').match, self.lines))
+
+    @property
+    def is_safe_to_colorize(self):
+        """
+        Public: Is the blob safe to colorize?
+
+        We use Pygments.rb for syntax highlighting blobs, which
+        has some quirks and also is essentially 'un-killable' via
+        normal timeout.  To workaround this we try to
+        carefully handling Pygments.rb anything it can't handle.
+
+        Return true or false
+        """
+        return not self.is_large and self.is_text and not self.is_high_ratio_of_long_lines
+
+    @property
+    def is_high_ratio_of_long_lines(self):
+        """
+        Internal: Does the blob have a ratio of long lines?
+        These types of files are usually going to make Pygments.rb
+        angry if we try to colorize them.
+
+        Return true or false
+        """
+        if self.loc == 0:
+            return False
+        return self.size / self.loc > 5000
+
+    @property
+    def is_viewable(self):
+        """
+        Public: Is the blob viewable?
+
+        Non-viewable blobs will just show a "View Raw" link
+
+        Return true or false
+        """
+        return not self.is_large and self.is_text
+
+    @property
+    def is_vendored(self):
+        """
+        Public: Is the blob in a vendored directory?
+
+        Vendored files are ignored by language statistics.
+
+        See "vendor.yml" for a list of vendored conventions that match
+        this pattern.
+
+        Return true or false
+        """
+        if VENDORED_REGEXP.search(self.name):
+            return True
+
+    @property
+    def lines(self):
+        """
+        Public: Get each line of data
+
+        Requires Blob#data
+
+        Returns an Array of lines
+        """
+        if hasattr(self, '_lines'):
+            return self._lines
+        if self.is_viewable and self.data:
+            self._lines = self.data.split(self.line_split_character, -1)
+        else:
+            self._lines = []
+        return self._lines
+
+    @property
+    def line_split_character(self):
+        """
+        Character used to split lines. This is almost always "\n" except when Mac
+        Format is detected in which case it's "\r".
+
+        Returns a split pattern string.
+        """
+        if hasattr(self, '_line_split_character'):
+            return self._line_split_character
+        if self.is_mac_format:
+            self._line_split_character = '\r'
+        else:
+            self._line_split_character = '\n'
+        return self._line_split_character
+
+    @property
+    def is_mac_format(self):
+        """
+        Public: Is the data in ** Mac Format **. This format uses \r (0x0d) characters
+        for line ends and does not include a \n (0x0a).
+
+        Returns true when mac format is detected.
+        """
+        if not self.is_viewable:
+            return 
+
+        data = self.data[0:4096]
+        if '\r' in data:
+            pos = data.index('\r')
+            return data[pos+1] != '\n'
+
+    @property
+    def is_generated(self):
+        """
+        Public: Is the blob a generated file?
+        
+        Generated source code is supressed in diffs and is ignored by
+        language statistics.
+
+        May load Blob#data
+
+        Return true or false
+        """
+        if hasattr(self, '_is_generated'):
+            return self._is_generated
+
+        def _data_func():
+            return self.data
+
+        self._is_generated = Generated.is_generated(self.name, _data_func)
+        return self._is_generated
+
+    @property
+    def is_indexable(self):
+        """
+        Public: Should the blob be indexed for searching?
+        
+        Excluded:
+          - Files over 0.1MB
+          - Non-text files
+          - Langauges marked as not searchable
+          - Generated source files
+
+        Please add additional test coverage to
+        `test/test_blob.rb#test_indexable` if you make any changes.
+
+        Return true or false
+        """
+        if self.size > 100 * 1024:
+            return False
+        elif self.is_binary:
+            return False
+        elif self.ext_name == '.txt':
+            return True
+        elif self.language is None:
+            return False
+        elif not self.language.is_searchable:
+            return False
+        elif self.is_generated:
+            return False
+        else:
+            return False
+
+
+    @property
+    def language(self):
+        """
+        Public: Detects the Language of the blob.
+
+        May load Blob#data
+
+        Returns a Language or nil if none is detected
+        """
+        if hasattr(self, '_language'):
+            return self._language
+
+        def data():
+            if self.is_binary_mime_type or self.is_binary:
+                return ''
+            return self.data
+        self._language = Language.detect(self.name, data, self.mode)
+        return self._language
+
+    @property
+    def lexer(self):
+        """
+        Internal: Get the lexer of the blob.
+
+        Returns a Lexer.
+        """
+        return self.language.lexer if self.language else lexers.find_lexer_class('Text only')
+
+    def colorize(self, options={}):
+        """
+        Public: Highlight syntax of blob
+
+        options - A Hash of options (defaults to {})
+
+        Returns html String
+        """
+        if not self.is_safe_to_colorize:
+            return 
+        return highlight(self.data, self.lexer(), HtmlFormatter(**options))
+
+    def colorize_without_wrapper(self, options={}):
+        """
+        Public: Highlight syntax of blob without the outer highlight div
+        """
+        text = self.colorize(options)
+        if text:
+            ret = re.compile(r'<div class="highlight"><pre>(.*?)<\/pre>\s*<\/div>', re.DOTALL).match(text)
+            if ret:
+                ret = ret.group(1)
+        else:
+            ret = ''
+        return ret
---
+++ tests/test_blob.py
@@ -0,0 +1,309 @@
+# -*- coding: utf-8 -*-
+
+from os.path import realpath, dirname, join
+from pygments.lexers import find_lexer_class
+from framework import LinguistTestBase, main
+from libs.file_blob import FileBlob
+from libs.samples import Samples
+
+DIR = dirname(dirname(realpath(__file__)))
+SAMPLES_PATH = join(DIR, "samples")
+
+colorize = """<div class="highlight"><pre><span class="k">module</span> <span class="nn">Foo</span>
+<span class="k">end</span>
+</pre></div>
+"""
+
+colorize_without_wrapper = """<span class="k">module</span> <span class="nn">Foo</span>
+<span class="k">end</span>
+"""
+
+class TestFileBob(LinguistTestBase):
+
+    def blob(self, name):
+        if not name.startswith('/'):
+            name = join(SAMPLES_PATH, name)
+        return FileBlob(name, SAMPLES_PATH)
+
+    def script_blob(self, name):
+        blob = self.blob(name)
+        blob.name = 'script'
+        return blob
+
+    def test_name(self):
+        assert 'foo.rb' == self.blob('foo.rb').name
+
+    def test_mime_type(self):
+        assert 'application/postscript' == self.blob('Binary/octocat.ai').mime_type
+        assert 'application/x-ruby' == self.blob("Ruby/grit.rb").mime_type
+        assert "application/x-sh" == self.blob("Shell/script.sh").mime_type
+        assert "application/xml" == self.blob("XML/bar.xml").mime_type
+        assert "audio/ogg" == self.blob("Binary/foo.ogg").mime_type
+        assert "text/plain" == self.blob("Text/README").mime_type
+
+    def test_content_type(self):
+        assert "application/pdf" == self.blob("Binary/foo.pdf").content_type
+        # assert "audio/ogg" == self.blob("Binary/foo.ogg").content_type
+        assert "image/png" == self.blob("Binary/foo.png").content_type
+        # assert "text/plain; charset=iso-8859-2" == self.blob("Text/README").content_type
+
+    def test_disposition(self):
+    #     assert "attachment; filename=foo+bar.jar" == self.blob("Binary/foo bar.jar").disposition
+    #     assert "attachment; filename=foo.bin" == self.blob("Binary/foo.bin").disposition
+    #     assert "attachment; filename=linguist.gem" == self.blob("Binary/linguist.gem").disposition
+    # FIXME runtime too lang
+    #     assert "attachment; filename=octocat.ai" == self.blob("Binary/octocat.ai").disposition
+        assert "inline" == self.blob("Text/README").disposition
+        assert "inline" == self.blob("Text/foo.txt").disposition
+        assert "inline" == self.blob("Ruby/grit.rb").disposition
+        assert "inline" == self. blob("Binary/octocat.png").disposition
+
+    def test_data(self):
+        assert "module Foo\nend\n" == self.blob("Ruby/foo.rb").data
+
+    def test_lines(self):
+        assert ["module Foo", "end", ""] == self.blob("Ruby/foo.rb").lines
+ 
+    def test_mac_format(self):
+        assert self.blob("Text/mac.txt").is_mac_format
+
+    def test_lines_mac_format(self):
+        assert ["line 1", "line 2", ""] == self.blob("Text/mac.txt").lines
+
+    def test_size(self):
+        assert 15 == self.blob("Ruby/foo.rb").size
+
+    def test_loc(self):
+        assert 3 == self.blob("Ruby/foo.rb").loc
+
+    def test_sloc(self):
+        assert 2 == self.blob("Ruby/foo.rb").sloc
+
+    def test_encoding(self):
+        pass
+        # assert "ISO-8859-2" == self.blob("Text/README").encoding
+        # assert "ISO-8859-1" == self.blob("Text/dump.sql").encoding
+        # assert "UTF-8" == self.blob("Text/foo.txt").encoding
+        # assert None == self.blob("Binary/dog.o").encoding
+
+    def test_binary(self):
+        # Large blobs aren't loaded
+        # large_blob = blob("git.exe")
+        # large_blob.instance_eval do
+        #   def data; end
+        # end
+        # assert large_blob.binary
+
+        # assert self.blob("Binary/git.deb").is_binary
+        # assert self.blob("Binary/git.exe").is_binary
+        # assert self.blob("Binary/hello.pbc").is_binary
+        # assert self.blob("Binary/linguist.gem").is_binary
+        # assert self.blob("Binary/octocat.ai").is_binary
+        assert self.blob("Binary/octocat.png").is_binary
+        # assert self.blob("Binary/zip").is_binary
+        assert not self.blob("Text/README").is_binary
+        assert not self.blob("Text/file.txt").is_binary
+        assert not self.blob("Ruby/foo.rb").is_binary
+        assert not self.blob("Perl/script.pl").is_binary
+
+    def test_text(self):
+        assert self.blob("Text/README").is_text
+        assert self.blob("Text/dump.sql").is_text
+        assert self.blob("Text/file.json").is_text
+        assert self.blob("Text/file.txt").is_text
+        assert self.blob("Text/md").is_text
+        assert self.blob("Shell/script.sh").is_text
+        assert self.blob("Text/txt").is_text
+
+    def test_image(self):
+        assert self.blob("Binary/octocat.gif").is_image
+        assert self.blob("Binary/octocat.jpeg").is_image
+        assert self.blob("Binary/octocat.jpg").is_image
+        assert self.blob("Binary/octocat.png").is_image
+        assert not self.blob("Binary/octocat.ai").is_image
+        assert not self.blob("Binary/octocat.psd").is_image
+
+    def test_solid(self):
+        assert self.blob("Binary/cube.stl").is_solid
+        assert self.blob("Text/cube.stl").is_solid
+
+    def test_viewable(self):
+        assert self.blob("Text/README").is_viewable
+        assert self.blob("Ruby/foo.rb").is_viewable
+        assert self.blob("Perl/script.pl").is_viewable
+        # assert not self.blob("Binary/linguist.gem").is_viewable
+        # FIXME runtime too long
+        # assert not self.blob("Binary/octocat.ai").is_viewable
+        assert not self.blob("Binary/octocat.png").is_viewable
+
+    def test_generated(self):
+        assert not self.blob("Text/README").is_generated
+
+        # Xcode project files
+        assert self.blob("XML/MainMenu.xib").is_generated
+        assert self.blob("Binary/MainMenu.nib").is_generated
+        assert self.blob("XML/project.pbxproj").is_generated
+
+        # Gemfile.locks
+        assert self.blob("Gemfile.lock").is_generated
+
+        # Generated .NET Docfiles
+        assert self.blob("XML/net_docfile.xml").is_generated
+
+        # Long line
+        assert not self.blob("JavaScript/uglify.js").is_generated
+
+        # Inlined JS, but mostly code
+        assert not self.blob("JavaScript/json2_backbone.js").is_generated
+
+        # Minified JS
+        assert not self.blob("JavaScript/jquery-1.6.1.js").is_generated
+        assert self.blob("JavaScript/jquery-1.6.1.min.js").is_generated
+        assert self.blob("JavaScript/jquery-1.4.2.min.js").is_generated
+
+        # CoffeeScript-is_generated JS
+        # TODO
+
+        # TypeScript-is_generated JS
+        # TODO
+
+        # PEG.js-is_generated parsers
+        assert self.blob("JavaScript/parser.js").is_generated
+
+        # These examples are too basic to tell
+        assert not self.blob("JavaScript/empty.js").is_generated
+        assert not self.blob("JavaScript/hello.js").is_generated
+
+        assert self.blob("JavaScript/intro-old.js").is_generated
+        assert self.blob("JavaScript/classes-old.js").is_generated
+
+        assert self.blob("JavaScript/intro.js").is_generated
+        assert self.blob("JavaScript/classes.js").is_generated
+
+        # Cython-is_generated C/C++
+        assert self.blob("C/sgd_fast.c").is_generated
+        assert self.blob("C++/wrapper_inner.cpp").is_generated
+
+    def test_vendored(self):
+        assert not self.blob("Text/README").is_vendored
+        assert not self.blob("ext/extconf.rb").is_vendored
+
+        # Node depedencies
+        assert  self.blob("node_modules/coffee-script/lib/coffee-script.js").is_vendored
+
+        # Rails vendor/
+        assert  self.blob("vendor/plugins/will_paginate/lib/will_paginate.rb").is_vendored
+
+        # C deps
+        assert  self.blob("deps/http_parser/http_parser.c").is_vendored
+        assert  self.blob("deps/v8/src/v8.h").is_vendored
+        
+        # Debian packaging
+        assert  self.blob("debian/cron.d").is_vendored
+
+        # Prototype
+        assert not self.blob("public/javascripts/application.js").is_vendored
+        assert  self.blob("public/javascripts/prototype.js").is_vendored
+        assert  self.blob("public/javascripts/effects.js").is_vendored
+        assert  self.blob("public/javascripts/controls.js").is_vendored
+        assert  self.blob("public/javascripts/dragdrop.js").is_vendored
+
+        # jQuery
+        assert  self.blob("jquery.js").is_vendored
+        assert  self.blob("public/javascripts/jquery.js").is_vendored
+        assert  self.blob("public/javascripts/jquery.min.js").is_vendored
+        assert  self.blob("public/javascripts/jquery-1.7.js").is_vendored
+        assert  self.blob("public/javascripts/jquery-1.7.min.js").is_vendored
+        assert  self.blob("public/javascripts/jquery-1.5.2.js").is_vendored
+        assert  self.blob("public/javascripts/jquery-1.6.1.js").is_vendored
+        assert  self.blob("public/javascripts/jquery-1.6.1.min.js").is_vendored
+        assert not self.blob("public/javascripts/jquery.github.menu.js").is_vendored
+
+        # MooTools
+        assert  self.blob("public/javascripts/mootools-core-1.3.2-full-compat.js").is_vendored
+        assert  self.blob("public/javascripts/mootools-core-1.3.2-full-compat-yc.js").is_vendored
+
+        # Dojo
+        assert  self.blob("public/javascripts/dojo.js").is_vendored
+
+        # MochiKit
+        assert  self.blob("public/javascripts/MochiKit.js").is_vendored
+
+        # YUI
+        assert  self.blob("public/javascripts/yahoo-dom-event.js").is_vendored
+        assert  self.blob("public/javascripts/yahoo-min.js").is_vendored
+        assert  self.blob("public/javascripts/yuiloader-dom-event.js").is_vendored
+
+        # LESS
+        assert  self.blob("public/javascripts/less-1.1.0.js").is_vendored
+        assert  self.blob("public/javascripts/less-1.1.0.min.js").is_vendored
+
+        # WYS editors
+        assert  self.blob("public/javascripts/ckeditor.js").is_vendored
+        assert  self.blob("public/javascripts/tiny_mce.js").is_vendored
+        assert  self.blob("public/javascripts/tiny_mce_popup.js").is_vendored
+        assert  self.blob("public/javascripts/tiny_mce_src.js").is_vendored
+
+        # Fabric
+        assert  self.blob("fabfile.py").is_vendored
+
+        # WAF
+        assert  self.blob("waf").is_vendored
+
+        # Visual Studio IntelliSense
+        assert  self.blob("Scripts/jquery-1.7-vsdoc.js").is_vendored
+
+        # Microsoft Ajax
+        assert  self.blob("Scripts/MicrosoftAjax.debug.js").is_vendored
+        assert  self.blob("Scripts/MicrosoftAjax.js").is_vendored
+        assert  self.blob("Scripts/MicrosoftMvcAjax.debug.js").is_vendored
+        assert  self.blob("Scripts/MicrosoftMvcAjax.js").is_vendored
+        assert  self.blob("Scripts/MicrosoftMvcValidation.debug.js").is_vendored
+        assert  self.blob("Scripts/MicrosoftMvcValidation.js").is_vendored
+
+        # jQuery validation plugin (MS bundles this with asp.net mvc)
+        assert  self.blob("Scripts/jquery.validate.js").is_vendored
+
+        # NuGet Packages
+        assert  self.blob("packages/Modernizr.2.0.6/Content/Scripts/modernizr-2.0.6-development-only.js").is_vendored
+
+    def test_indexable(self):
+        # assert self.blob("Ruby/foo.rb").is_indexable
+        assert not self.blob("Text/defu.nkt").is_indexable
+        assert not self.blob("Text/dump.sql").is_indexable
+        assert not self.blob("Binary/github.po").is_indexable
+        assert not self.blob("Binary/linguist.gem").is_indexable
+
+        # large binary blobs should fail on size check first, not call
+        # into charlock_holmes and alloc big buffers for testing encoding
+        # b = blob("Binary/octocat.ai")
+        # b.expects(:binary?).never
+        # assert !b.indexable?
+
+    def test_language(self):
+        def _check_lang(sample):
+            blob = self.blob(sample['path'])
+            assert blob.language, 'No language for %s' % sample['path']
+            assert sample['language'] == blob.language.name, blob.name
+
+        Samples.each(_check_lang)
+
+    def test_lexer(self):
+        assert find_lexer_class('Ruby') == self.blob("Ruby/foo.rb").lexer
+
+    def test_colorize(self):
+        assert colorize == self.blob("Ruby/foo.rb").colorize()
+
+    def test_colorize_without_wrapper(self):
+        assert colorize_without_wrapper == self.blob("Ruby/foo.rb").colorize_without_wrapper()
+
+    def test_colorize_does_skip_minified_files(self):
+        assert None == self.blob("JavaScript/jquery-1.6.1.min.js").colorize()
+
+    # Pygments.rb was taking exceeding long on this particular file
+    def test_colorize_doesnt_blow_up_with_files_with_high_ratio_of_long_lines(self):
+        assert None == self.blob("JavaScript/steelseries-min.js").colorize()
+
+
+if __name__ == '__main__':
+    main()
